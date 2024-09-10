7882443a84c8c1d5dce55d86303a451eb170c8d3
Filip <fillip.detterfelt@gmail.com>
2024-09-10 20:07:25 +0200

    Built the ui for minusräknaren

---
+++ src/main/java/org/example/gamecenter/controller/MinusController.java
@@ -0,0 +1,13 @@
+package org.example.gamecenter.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MinusController {
+
+    @GetMapping("/minus")
+    public String minus() {
+        return "minus";
+    }
+}
