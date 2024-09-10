82638989955419fd335f3db3bb2a708d6ff39e33
Filip <fillip.detterfelt@gmail.com>
2024-09-10 10:57:33 +0200

    Added amazing program

---
+++ src/main/java/controller/HelloWorld.java
@@ -0,0 +1,9 @@
+package controller;
+
+import org.springframework.http.converter.json.GsonBuilderUtils;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HELLO WORLD");
+    }
+}
