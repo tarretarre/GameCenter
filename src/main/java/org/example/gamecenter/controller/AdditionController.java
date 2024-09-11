e1be069e6c464c998a6f0599d57b937d2482cc30
Robin Blondin <robin.blondin@protonmail.com>
2024-09-11 15:29:15 +0200

    Added controller methods

--- src/main/java/org/example/gamecenter/controller/AdditionController.java
+++ src/main/java/org/example/gamecenter/controller/AdditionController.java
@@ -1,11 +1,18 @@
 package org.example.gamecenter.controller;
+
+import lombok.RequiredArgsConstructor;
 import org.example.gamecenter.dto.AdditionGameDto;
+import org.example.gamecenter.service.AdditionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
+@RequiredArgsConstructor
 public class AdditionController {
+    private final AdditionService additionService;
 
     @GetMapping("/addition")
     public String addition(Model model) {
@@ -19,4 +26,25 @@ public class AdditionController {
 
         return "addition";
     }
+
+    @PostMapping("/addition")
+    public String additionPost(AdditionGameDto gameDto, RedirectAttributes rda) {
+        if(!gameDto.isStarted()) {
+            additionService.startGame(gameDto);
+            rda.addFlashAttribute("gameDto", gameDto);
+            return "redirect:/addition";
+        }
+
+        additionService.nextRound(gameDto);
+        rda.addFlashAttribute("gameDto", gameDto);
+        return "redirect:/addition";
+    }
+
+    @PostMapping("/addition/answer_check")
+    public String answerCheck(AdditionGameDto gameDto, RedirectAttributes rda) {
+        additionService.checkAnswer(gameDto);
+        rda.addFlashAttribute("gameDto", gameDto);
+        return "redirect:/addition";
+    }
+
 }
