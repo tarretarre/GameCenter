46b67b8473edfec20d1cc75262bd6d5286e1545d
Filip <fillip.detterfelt@gmail.com>
2024-09-11 12:27:03 +0200

    Test working

--- src/main/java/org/example/gamecenter/controller/MinusController.java
+++ src/main/java/org/example/gamecenter/controller/MinusController.java
@@ -3,12 +3,12 @@ package org.example.gamecenter.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class MinusController {
 
     @GetMapping("/minus")
-    @ResponseBody
     public String minus() {
         return "minus";
     }
--- src/test/java/org/example/gamecenter/MinusControllerTest.java
+++ src/test/java/org/example/gamecenter/MinusControllerTest.java
@@ -1,16 +1,21 @@
 package org.example.gamecenter;
 
-import org.example.gamecenter.controller.MinusController;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-@WebMvcTest(MinusController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
 public class MinusControllerTest {
 
     @Autowired()
@@ -19,7 +24,8 @@ public class MinusControllerTest {
     @Test
     public void testMinusReturnMinus() throws Exception {
         mockMvc.perform(get("/minus"))
+                .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().string("minus"));
+                .andExpect(view().name("minus"));
     }
 }
