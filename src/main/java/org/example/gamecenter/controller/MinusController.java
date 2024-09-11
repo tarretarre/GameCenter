b2be90d6729d01ddde44b54517ebad8574ba6fd8
Filip <fillip.detterfelt@gmail.com>
2024-09-11 12:29:38 +0200

    Small fix

--- src/main/java/org/example/gamecenter/controller/MinusController.java
+++ src/main/java/org/example/gamecenter/controller/MinusController.java
@@ -2,8 +2,6 @@ package org.example.gamecenter.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class MinusController {
