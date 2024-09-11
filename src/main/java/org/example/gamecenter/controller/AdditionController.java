1e1c8ceea610f57f72c687962ac583df3dbc104f
Robin Blondin <robin.blondin@protonmail.com>
2024-09-11 15:05:34 +0200

    Added model checks

--- src/main/java/org/example/gamecenter/controller/AdditionController.java
+++ src/main/java/org/example/gamecenter/controller/AdditionController.java
@@ -1,13 +1,22 @@
 package org.example.gamecenter.controller;
+import org.example.gamecenter.dto.AdditionGameDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdditionController {
 
     @GetMapping("/addition")
-    public String addition() {
-        System.out.println("addition() method is called");
+    public String addition(Model model) {
+        if(!model.containsAttribute("gameDto")) {
+            model.addAttribute("gameDto", new AdditionGameDto());
+        }
+
+        if(!model.containsAttribute("message")) {
+            model.addAttribute("message", "");
+        }
+
         return "addition";
     }
 }
