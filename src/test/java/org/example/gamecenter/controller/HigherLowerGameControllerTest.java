780dd71259b44e14717ad2e3c4b526d35a70015f
MSpeiner <michael.s_97@hotmail.com>
2024-09-11 12:18:48 +0200

    Fixed most warnings

--- src/test/java/org/example/gamecenter/controller/HigherLowerGameControllerTest.java
+++ src/test/java/org/example/gamecenter/controller/HigherLowerGameControllerTest.java
@@ -7,11 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
