e702c6ed457e152bbaf28cb0cabbd945e8caf0de
Robin Blondin <robin.blondin@protonmail.com>
2024-09-12 08:18:32 +0200

    implemented the use of HttpSession for gameDTO

--- src/main/java/org/example/gamecenter/controller/AdditionController.java
+++ src/main/java/org/example/gamecenter/controller/AdditionController.java
@@ -1,11 +1,13 @@
 package org.example.gamecenter.controller;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.example.gamecenter.dto.AdditionGameDto;
 import org.example.gamecenter.service.AdditionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -15,35 +17,72 @@ public class AdditionController {
     private final AdditionService additionService;
 
     @GetMapping("/addition")
-    public String addition(Model model) {
-        if(!model.containsAttribute("gameDto")) {
-            model.addAttribute("gameDto", new AdditionGameDto());
+    public String addition(Model model, HttpSession session) {
+        AdditionGameDto gameDto = (AdditionGameDto) session.getAttribute("gameDto");
+
+        if (gameDto == null) {
+            gameDto = new AdditionGameDto();
+            session.setAttribute("gameDto", gameDto);
         }
+
+        model.addAttribute("gameDto", gameDto);
 
         if(!model.containsAttribute("message")) {
             model.addAttribute("message", "");
+        }
+
+        if (!model.containsAttribute("answerCheck")) {
+            model.addAttribute("answerCheck", false);
+        }
+
+        if (!model.containsAttribute("gameOver")) {
+            model.addAttribute("gameOver", false);
         }
 
         return "addition";
     }
 
     @PostMapping("/addition")
-    public String additionPost(AdditionGameDto gameDto, RedirectAttributes rda) {
+    public String additionPost(AdditionGameDto formDto, HttpSession session, RedirectAttributes rda) {
+        AdditionGameDto gameDto = (AdditionGameDto) session.getAttribute("gameDto");
+
+        if (gameDto == null) {
+            gameDto = formDto;
+        } else if (formDto != null) {
+            gameDto.setUserAnswer(formDto.getUserAnswer());
+            gameDto.setTotalRounds(formDto.getTotalRounds());
+        }
+
+        if(gameDto.getCurrentRound() == gameDto.getTotalRounds()) {
+            session.setAttribute("gameDto", gameDto);
+            rda.addFlashAttribute("gameOver", true);
+            return "redirect:/addition";
+        }
+
         if(!gameDto.isStarted()) {
             additionService.startGame(gameDto);
-            rda.addFlashAttribute("gameDto", gameDto);
+            gameDto.print();
+            session.setAttribute("gameDto", gameDto);
             return "redirect:/addition";
         }
 
         additionService.nextRound(gameDto);
-        rda.addFlashAttribute("gameDto", gameDto);
+        session.setAttribute("gameDto", gameDto);
         return "redirect:/addition";
     }
 
     @PostMapping("/addition/answer_check")
-    public String answerCheck(AdditionGameDto gameDto, RedirectAttributes rda) {
-        additionService.checkAnswer(gameDto);
-        rda.addFlashAttribute("gameDto", gameDto);
+    public String answerCheck(@ModelAttribute(name = "gameDto") AdditionGameDto formDto, RedirectAttributes rda, HttpSession session) {
+        AdditionGameDto gameDto = (AdditionGameDto) session.getAttribute("gameDto");
+
+        if(gameDto != null) {
+            gameDto.setUserAnswer(formDto.getUserAnswer());
+            additionService.checkAnswer(gameDto);
+            session.setAttribute("gameDto", gameDto);
+            gameDto.print();
+        }
+
+        rda.addFlashAttribute("answerCheck", true);
         return "redirect:/addition";
     }
 
