72c574ef5d4d7cec23d69707dd64de1de5ccbea0
Robin Blondin <robin.blondin@protonmail.com>
2024-09-12 08:19:01 +0200

    Added print for troubleshooting

--- src/main/java/org/example/gamecenter/dto/AdditionGameDto.java
+++ src/main/java/org/example/gamecenter/dto/AdditionGameDto.java
@@ -3,38 +3,40 @@ package org.example.gamecenter.dto;
 import lombok.Data;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Data
 
 public class AdditionGameDto {
     private int totalRounds;
     private int currentRound;
-    private int correct;
+    private int correctAnswers;
     private int userAnswer;
-    private Map<String, Integer> questionsAndCorrectAnswers;
+    private List<String> questions;
+    private List<Integer> answers;
     private List<List<Integer>> answerChoices;
     private boolean started;
 
-    public AdditionGameDto(int totalRounds) {
-        this.totalRounds = totalRounds;
-        this.currentRound = 1;
-        this.correct = 0;
-        this.userAnswer = -1;
-        this.questionsAndCorrectAnswers = new HashMap<>();
-        this.answerChoices = new ArrayList<>();
-        this.started = true;
-    }
 
     public AdditionGameDto() {
         this.totalRounds = 0;
         this.currentRound = 0;
-        this.correct = 0;
+        this.correctAnswers = 0;
         this.userAnswer = -1;
-        this.questionsAndCorrectAnswers = new HashMap<>();
+        this.questions = new ArrayList<>();
+        this.answers = new ArrayList<>();
         this.answerChoices = new ArrayList<>();
         this.started = false;
+    }
+
+    public void print() {
+        System.out.println("Total Rounds: " + totalRounds);
+        System.out.println("Current Round: " + currentRound);
+        System.out.println("Correct Answers: " + correctAnswers);
+        System.out.println("User Answer: " + userAnswer);
+        System.out.println("Questions: " + questions);
+        System.out.println("Answers: " + answers);
+        System.out.println("Answer Choices: " + answerChoices);
+        System.out.println("Started: " + started);
     }
 }
