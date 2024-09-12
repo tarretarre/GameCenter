fe73f6ccf2d856f86afdf18e130dcd10fe1f3557
Robin Blondin <robin.blondin@protonmail.com>
2024-09-12 14:50:40 +0200

    Changed typing of fields

--- src/main/java/org/example/gamecenter/dto/AdditionGameDto.java
+++ src/main/java/org/example/gamecenter/dto/AdditionGameDto.java
@@ -8,10 +8,10 @@ import java.util.List;
 @Data
 
 public class AdditionGameDto {
-    private int totalRounds;
+    private Integer totalRounds;
     private int currentRound;
     private int correctAnswers;
-    private int userAnswer;
+    private Integer userAnswer;
     private List<String> questions;
     private List<Integer> answers;
     private List<List<Integer>> answerChoices;
@@ -22,21 +22,10 @@ public class AdditionGameDto {
         this.totalRounds = 0;
         this.currentRound = 0;
         this.correctAnswers = 0;
-        this.userAnswer = -1;
+        this.userAnswer = null;
         this.questions = new ArrayList<>();
         this.answers = new ArrayList<>();
         this.answerChoices = new ArrayList<>();
         this.started = false;
-    }
-
-    public void print() {
-        System.out.println("Total Rounds: " + totalRounds);
-        System.out.println("Current Round: " + currentRound);
-        System.out.println("Correct Answers: " + correctAnswers);
-        System.out.println("User Answer: " + userAnswer);
-        System.out.println("Questions: " + questions);
-        System.out.println("Answers: " + answers);
-        System.out.println("Answer Choices: " + answerChoices);
-        System.out.println("Started: " + started);
     }
 }
