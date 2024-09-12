51bc0210b2aa4813c3059babe99c591355e9c010
Robin Blondin <robin.blondin@protonmail.com>
2024-09-12 20:26:15 +0200

    working on tests

--- src/test/java/org/example/gamecenter/controller/AdditionControllerTest.java
+++ src/test/java/org/example/gamecenter/controller/AdditionControllerTest.java
@@ -1,17 +1,25 @@
 package org.example.gamecenter.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.gamecenter.dto.AdditionGameDto;
+import org.example.gamecenter.service.AdditionService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
@@ -21,20 +29,46 @@ class AdditionControllerTest {
     @Autowired
     private  MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private AdditionService additionService;
+
     @Test
-    void addition() throws Exception {
-        mockMvc.perform(get("/addition"))
+    void additionTest_shouldContainModelAttributes() throws Exception {
+        AdditionGameDto gameDto = new AdditionGameDto();
+        MockHttpSession session = new MockHttpSession();
+        session.setAttribute("gameDto", gameDto);
+
+
+        mockMvc.perform(get("/addition").session(session))
                 .andDo(print())
                 .andExpect(status().isOk())
+                .andExpect(model().attributeExists("gameDto"))
+                .andExpect(model().attributeExists("gameOver"))
+                .andExpect(model().attributeExists("answerCheck"))
+                .andExpect(model().attributeExists("lastRound"))
+                .andExpect(model().attribute("gameOver", false))
+                .andExpect(model().attribute("answerCheck", false))
+                .andExpect(model().attribute("lastRound", false))
                 .andExpect(view().name("addition"));
     }
 
     @Test
-    void testAddition() {
-    }
+    void additionPost_startNewGame() throws Exception {
+        int totalRounds = 5;
+        MockHttpSession session = new MockHttpSession();
+        AdditionGameDto gameDto = new AdditionGameDto();
+        gameDto.setTotalRounds(totalRounds);
 
-    @Test
-    void additionPost() {
+        when(additionService.isFirstRound(gameDto)).thenReturn(true);
+
+        mockMvc.perform(post("/addition").session(session).param("totalRounds", Integer.toString(totalRounds)))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/addition"))
+
     }
 
     @Test
