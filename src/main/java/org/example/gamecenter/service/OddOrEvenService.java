a5b393f299b9ce5daca19a89b6ac249ae1015d5a
Tuan Nguyen <tuan.nguyen@digitalengine.se>
2024-09-13 16:22:09 +0200

    Added more tests Service and method to Service

--- src/main/java/org/example/gamecenter/service/OddOrEvenService.java
+++ src/main/java/org/example/gamecenter/service/OddOrEvenService.java
@@ -13,8 +13,30 @@ public class OddOrEvenService {
 
     private Random random = new Random();
     private int randomNumber;
+    private int correctChoices = 0;
+    private int roundCount = 0;
 
     public void generateRandomNumber() {
         randomNumber = random.nextInt(100) + 1;
+    }
+
+    public void startNewGame() {
+        resetGame();
+        generateRandomNumber();
+    }
+
+    private void resetGame() {
+        correctChoices = 0;
+        roundCount = 0;
+    }
+
+
+    public boolean isGameOver() {
+        return roundCount >= 5;
+    }
+
+    public boolean checkChoice(String choice) {
+        boolean isEven = (randomNumber % 2 == 0);
+        return (isEven && choice.equalsIgnoreCase("Even")) || (!isEven && choice.equalsIgnoreCase("Odd"));
     }
 }
--- src/test/java/org/example/gamecenter/service/OddOrEvenServiceTest.java
+++ src/test/java/org/example/gamecenter/service/OddOrEvenServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
@@ -16,7 +17,28 @@ public class OddOrEvenServiceTest {
     public void testGenerateRandomNumberWithinRange() {
         oddOrEvenService.generateRandomNumber();
         int randomNumber = oddOrEvenService.getRandomNumber();
-
         assertTrue(randomNumber >= 1 && randomNumber <= 100);
     }
+
+    @Test
+    public void testStartNewGameShouldResetGameAndGenerateRandomNumber() {
+        oddOrEvenService.startNewGame();
+        assertEquals(0, oddOrEvenService.getCorrectChoices());
+        assertEquals(0, oddOrEvenService.getRoundCount());
+        assertTrue(oddOrEvenService.getRandomNumber() > 0);
+    }
+
+    @Test
+    public void testCheckEvenChoice() {
+        oddOrEvenService.setRandomNumber(12);
+        assertTrue(oddOrEvenService.checkChoice("even"));
+    }
+
+    @Test
+    public void testCheckOddChoice() {
+        oddOrEvenService.setRandomNumber(11);
+        assertTrue(oddOrEvenService.checkChoice("odd"));
+    }
+
+
 }
