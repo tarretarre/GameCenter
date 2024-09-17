6a1df5a11181143fa0ee980557b20c5980b89183
Tuan Nguyen <tuan.nguyen@digitalengine.se>
2024-09-17 12:01:09 +0200

    Major space reversed

--- src/main/java/org/example/gamecenter/controller/OddOrEven.java
+++ src/main/java/org/example/gamecenter/controller/OddOrEven.java
@@ -8,7 +8,7 @@ public class OddOrEven {
 
     @GetMapping("/oddOrEven")
     public String oddOrEvenGame() {
-        System.out.println("Game Odd or Even  opened");
+        System.out.println("Game Odd or Even opened");
         return "oddOrEven";
     }
 }
