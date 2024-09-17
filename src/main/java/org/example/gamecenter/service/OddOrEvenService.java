7fb8c7ead06cc86f9457cec53c6625d90c37658c
Tuan Nguyen <tuan.nguyen@digitalengine.se>
2024-09-17 12:41:27 +0200

    Removed @Configuration in Service

--- src/main/java/org/example/gamecenter/service/OddOrEvenService.java
+++ src/main/java/org/example/gamecenter/service/OddOrEvenService.java
@@ -2,14 +2,12 @@ package org.example.gamecenter.service;
 
 import lombok.Data;
 import org.example.gamecenter.DTO.OddOrEvenDTO;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 
 import java.util.Random;
 
 @Data
 @Service
-@Configuration
 public class OddOrEvenService {
 
     private Random random = new Random();
